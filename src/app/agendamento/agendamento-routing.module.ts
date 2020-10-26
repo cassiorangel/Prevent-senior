3778941430b666671f79124c887a8adda019fd87
Cassio Rangel Fernandes <cassio.rfernandes@gmail.com>
2020-10-26 19:45:43 -0300

    lazy load implementado

--- src/app/agendamento/agendamento-routing.module.ts
+++ src/app/agendamento/agendamento-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AgendarComponent } from './agendar/agendar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: AgendarComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'agendar',
+    loadChildren: () => import('./agendamento/agendamento.module').then(m => m.AgendamentoModule)
+  },
+  {
+    path: '',
+    redirectTo: '/agendar',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
