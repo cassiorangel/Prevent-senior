5ba994b081b4dc6749055d409e15e47445c6836d
Cassio Rangel Fernandes <cassio.rfernandes@gmail.com>
2020-10-27 02:54:33 -0300

    Ajuste novo componente

--- src/app/agendamento/agendamento.module.ts
+++ src/app/agendamento/agendamento.module.ts
@@ -1,15 +1,29 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatSliderModule } from '@angular/material/slider';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AgendamentoRoutingModule } from './agendamento-routing.module';
 import { AgendarComponent } from './agendar/agendar.component';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatIconModule} from '@angular/material/icon';
+import {NgxMaterialTimepickerModule} from 'ngx-material-timepicker';
+
 
 
 @NgModule({
   declarations: [AgendarComponent],
   imports: [
     CommonModule,
+    MatSliderModule,
+    MatIconModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    NgxMaterialTimepickerModule,
+    MatFormFieldModule,
     AgendamentoRoutingModule
-  ]
+  ],
+  
 })
 export class AgendamentoModule { }
--- src/app/agendamento/agendar/agendar.component.ts
+++ src/app/agendamento/agendar/agendar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-agendar',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AgendarComponent implements OnInit {
 
-  constructor() { }
+  profileForm = this.fb.group({
+    horario: [null],
+    lastName: [null]
+  });
+
+  constructor(
+    private fb: FormBuilder
+  ) { }
 
   ngOnInit(): void {
+  }
+  onSubmit() {
+    console.log(this.profileForm.value)
   }
 
 }
