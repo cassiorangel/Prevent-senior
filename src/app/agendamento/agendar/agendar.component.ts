dd1cfef1c947659128a65484a07f78e1cc849167
cassio fernandes <cassio.rfernandes@gmail.com>
2023-09-26 23:49:03 -0300

    feat: ajuste

--- src/app/agendamento/agendar/agendar.component.ts
+++ src/app/agendamento/agendar/agendar.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
-import * as XLSX from 'ts-xlsx';
+import * as XLSX from 'xlsx';
+
+type AOA = any[][];
+
 
 @Component({
   selector: 'app-agendar',
@@ -8,6 +11,11 @@ import * as XLSX from 'ts-xlsx';
   styleUrls: ['./agendar.component.scss']
 })
 export class AgendarComponent implements OnInit {
+
+  data: any = [[1, 2], [3, 4]];
+  wopts: XLSX.WritingOptions = { bookType: 'xlsx', type: 'array' };
+  fileName: string = 'SheetJS.xlsx';
+
 
   arrayBuffer: any;
   file: File;
@@ -28,32 +36,28 @@ export class AgendarComponent implements OnInit {
     console.log(this.profileForm.value)
   }
 
-  incomingfile(event) {
-    this.file = event.target.files[0];
+  incomingfile(evt: any) {
+  /* wire up file reader */
+  const target: DataTransfer = <DataTransfer>(evt.target);
+  if (target.files.length !== 1) throw new Error('Cannot use multiple files');
+  const reader: FileReader = new FileReader();
+  reader.onload = (e: any) => {
+    /* read workbook */
+    const bstr: string = e.target.result;
+    const wb: XLSX.WorkBook = XLSX.read(bstr, { type: 'binary' });
+
+    /* grab first sheet */
+    const wsname: string = wb.SheetNames[0];
+    const ws: XLSX.WorkSheet = wb.Sheets[wsname];
+
+    /* save data */
+    this.data = <AOA>(XLSX.utils.sheet_to_json(ws, { header: 1 }));
+    console.log(this.data);
+  };
+  reader.readAsBinaryString(target.files[0]);
   }
 
   upload() {
-
-    let extArquivoTypo = this.file['name'].split('.').pop();
-
-    let extPermitidas = ['xlsx'];
-
-    if (typeof extPermitidas.find((ext) => { return extArquivoTypo == ext; }) == 'undefined') {
-      alert('Extensão "' + extArquivoTypo + '" não permitida!');
-    } else {
-      let fileReader = new FileReader();
-      fileReader.onload = (e) => {
-        this.arrayBuffer = fileReader.result;
-        var data = new Uint8Array(this.arrayBuffer);
-        var arr = new Array();
-        for (var i = 0; i != data.length; ++i) arr[i] = String.fromCharCode(data[i]);
-        var bstr = arr.join("");
-        var workbook = XLSX.read(bstr, { type: "binary" });
-        var first_sheet_name = workbook.SheetNames[0];
-        var worksheet = workbook.Sheets[first_sheet_name];
-        console.log('w', XLSX.utils.sheet_to_json(worksheet, { raw: true }));
-      }
-      fileReader.readAsArrayBuffer(this.file);
-    }
+//    XLSX.set_cptable(cpexcel);
   }
 }
